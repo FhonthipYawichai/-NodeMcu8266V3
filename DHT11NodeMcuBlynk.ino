5d5a89dd53a30c5ff40c888c516523fcad6db5dd
FHONTHIP.Y <64491388+FhonthipYawichai@users.noreply.github.com>
2020-10-09 02:48:13 +0700

    DHT11_NodeMcu_Blynk

---
+++ DHT11NodeMcuBlynk.ino
@@ -0,0 +1,59 @@
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>
+#include <DHT.h>
+#include<Wire.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "2a5hmMSNiwAYZZBcN2h5ACj5ZTZa7hcU"; //Enter the Auth code which was send by Blink
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Soynami";  //Enter your WIFI Name
+char pass[] = "dinodino";  //Enter your WIFI Password
+
+#define DHTPIN 2          // Digital pin 4
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11     // DHT 11
+//#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
+//#define DHTTYPE DHT21   // DHT 21, AM2301
+
+DHT dht(DHTPIN, DHTTYPE);
+SimpleTimer timer;
+
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, h);  //V5 is for Humidity
+  Blynk.virtualWrite(V6, t);  //V6 is for Temperature
+}
+
+void setup()
+{
+  Serial.begin(9600); // See the connection status in Serial Monitor
+  Blynk.begin(auth, ssid, pass);
+
+  dht.begin();
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run(); // Initiates Blynk
+  timer.run(); // Initiates SimpleTimer
+}
